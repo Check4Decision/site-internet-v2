7559e67a35df37c9765633140070c6ea6474ffa5
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2022-05-13 15:00:55 +0000

    Publications page

--- config.php
+++ config.php
@@ -6,5 +6,5 @@ return [
     'title' => 'Check4Decision',
     'language' => 'fr',
     'description' => "Check4Decision: Projet de recherche financé par le CEA-MITIC, qui traite les questions relatives à l'automatisation de la collecte et l'agrégation de données mais aussi de la vérification des faits (fact-checking) dans le contexte web journalistique.",
-    'collections' => ['axes', 'responsables', 'stagiaires'],
+    'collections' => ['axes', 'responsables', 'stagiaires', 'publications'],
 ];
--- source/publications.blade.php
+++ source/publications.blade.php
@@ -3,17 +3,18 @@
 @section('title', 'Publications')
 
 @section('main')
-    <section class="text-center pt-10 pb-20">
-        <div class="mx-auto space-y-24" style="max-width: 50rem;">
-            <h2 class="text-4xl sm:text-5xl font-koulen text-sky-900">Publications scientifiques</h2>
+    <section class="py-10">
+        <div class="mx-10 xl:mx-auto space-y-12" style="max-width: 80rem;">
+            <h2 class="text-4xl sm:text-5xl font-koulen text-sky-900 text-center">Publications scientifiques </h2>
 
-            <div class="grid grid-cols-2 place-items-center gap-6 sm:grid-cols-4">
-                <img class="w-28 h-28" src="/assets/images/ut.png" alt="Logo Université de Thiès">
-                <img class="w-28 h-28" src="/assets/images/utt.png" alt="Logo Université de Technologie de Troyes">
-                <img class="w-28 h-28" src="/assets/images/ceamitic.png" alt="Logo CEA-MITIC">
-                <img class="w-28 h-28" src="/assets/images/ucao.png"
-                    alt="Logo Université Catholique de l'Afrique de l'Ouest">
-            </div>
+            <ul class="divide-y-2 divide-gray-100">
+                @foreach ($publications as $publication)
+                    <li class="py-4 text-gray-900">
+                        <!-- Remove p tags -->
+                        {{ \Illuminate\Support\Str::between($publication->getContent(), '<p>', '</p>') }}
+                    </li>
+                @endforeach
+            </ul>
         </div>
     </section>
 @endsection
