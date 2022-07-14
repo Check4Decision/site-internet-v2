2260ed544bdf305b9e6047b9b5fd642e1878cf53
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2022-07-14 12:15:34 +0000

    Refactor & reorder head metas

--- source/_partials/head.blade.php
+++ source/_partials/head.blade.php
@@ -3,12 +3,12 @@
 <meta name="description" content="{{ $page->description }}">
 
 {{-- Facebook Open Graph --}}
+<meta property="og:image" content="https://check4decision.univ-thies.sn/assets/images/og-image.jpg">
 <meta property="og:image:height" content="150">
 <meta property="og:image:width" content="286">
 <meta property="og:title" content="{{ $page->title }}">
 <meta property="og:description" content="Site web du projet Check4Decision">
 <meta property="og:url" content="https://check4decision.univ-thies.sn/">
-<meta property="og:image" content="https://check4decision.univ-thies.sn/assets/images/og-image.jpg">
 
 {{-- Twitter Card --}}
 <meta name="twitter:card" content="summary">
@@ -18,11 +18,11 @@
 <link rel="apple-touch-icon" sizes="180x180" href="/assets/images/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="/assets/images/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="/assets/images/favicon-16x16.png">
-<link rel="manifest" href="/assets/images/site.webmanifest">
+<link rel="manifest" href="/site.webmanifest">
 <link rel="mask-icon" href="/assets/images/safari-pinned-tab.svg" color="#5bbad5">
 <link rel="shortcut icon" href="/assets/images/favicon.ico">
 <meta name="msapplication-TileColor" content="#da532c">
-<meta name="msapplication-config" content="/assets/images/browserconfig.xml">
+<meta name="msapplication-config" content="/browserconfig.xml">
 <meta name="theme-color" content="#ffffff">
 
 <title>{{ $page->title }} - @yield('title')</title>
