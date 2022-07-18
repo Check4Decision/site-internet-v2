9e3a9e80727911944aa2ab4d047acf83f94652b6
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2022-07-18 11:22:20 +0000

    Redesign hover and active states of navigation links

--- source/_components/navigation-item.blade.php
+++ source/_components/navigation-item.blade.php
@@ -1,9 +1,9 @@
 @props(['isActive' => false])
 
 <a {{ $attributes }} @class([
-    'text-white sm:text-gray-800' => !$isActive,
-    'text-white underline decoration-white decoration-2 underline-offset-4 sm:text-sky-900 sm:no-underline' => $isActive,
-    'tracking-wider font-semibold sm:hover:text-sky-900',
+    'text-white sm:text-gray-700' => !$isActive,
+    'text-white underline decoration-white decoration-2 underline-offset-4 sm:no-underline sm:bg-sky-600/10 sm:text-sky-700' => $isActive,
+    'tracking-wider font-semibold sm:p-2 sm:rounded-md sm:hover:bg-sky-600/10 sm:hover:text-sky-700',
 ])>
     {{ $slot }}
 </a>
