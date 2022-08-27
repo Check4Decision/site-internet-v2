ee4df368c156f775d9079bd7ed521899d28d41d3
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2022-08-27 11:35:58 +0000

    Fix styles rebuilding in an infinite loop

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,16 @@
 module.exports = {
-    content: ["source/**/*.blade.php", "source/**/*.md", "source/**/*.html"],
+    // content: ["source/**/*.blade.php", "source/**/*.md", "source/**/*.html"],
+    content: [
+        "./source/index.blade.php",
+        "./source/publications.blade.php",
+        "./source/_axes/*.md",
+        "./source/_components/*.blade.php",
+        "./source/_layouts/*.blade.php",
+        "./source/_partials/*.blade.php",
+        "./source/_publications/*.md",
+        "./source/_responsables/*.md",
+        "./source/_stagiaires/*.md",
+    ],
     theme: {
         extend: {
             fontFamily: {
